d3bdd22ffda52551f101ccbbab7c2d1581e7c215
andersonwr <35141647+andersonwr@users.noreply.github.com>
2018-11-14 14:57:35 -0500

    Updated isolation.py from piazza

--- aqua.py
+++ aqua.py
@@ -1,6 +1,16 @@
-from isolation.py import *
+from isolation import *
+
 
 class Aqua(Player):
-    def __init__(self, name, token):
-        super.__init__(self, name, token)
 
+    def __init__(self, name, token):
+        super(Aqua, self).__init__(name, token)
+
+    def take_turn(self, board):
+        """
+        Make a move on the Isolation board
+        :param board: a Board object
+        :return: a Move object
+        """
+        # Each subclass must implement this method
+        raise NotImplementedError
