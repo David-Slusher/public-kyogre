31a1c8ac48d786c5c58d96b08e0c885034f12744
andersonwr <35141647+andersonwr@users.noreply.github.com>
2018-11-19 11:36:17 -0500

    Incremental progress

--- aqua.py
+++ aqua.py
@@ -2,7 +2,6 @@ from isolation import *
 
 
 class Aqua(Player):
-
     def __init__(self, name, token):
         super(Aqua, self).__init__(name, token)
 
@@ -14,3 +13,36 @@ class Aqua(Player):
         """
         # Each subclass must implement this method
         raise NotImplementedError
+
+
+class Strategy:
+    def __init__(self):
+        return
+
+    def move(self, board):
+        """
+        Returns the id of the space to move to
+        :param board: a Board object
+        :return: to_space_id
+        """
+        raise NotImplementedError
+
+    def push(self, board):
+        """
+        Returns the id of the space to push
+        :param board: a Board object
+        :return: push_space_id
+        """
+        raise NotImplementedError
+
+
+class LateStrat(Strategy):
+
+    def __init__(self):
+        super(LateStrat, self).__init__()
+
+
+class EarlyStrat(Strategy):
+
+    def __init__(self):
+        super(EarlyStrat, self).__init__()
